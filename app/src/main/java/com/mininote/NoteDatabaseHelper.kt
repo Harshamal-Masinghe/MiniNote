feaa81180700dc9257496c83d87caaa0fcdb5677
Harsha <it22577306@my.sliit.lk>
2024-05-11 23:45:11 +0530

    Add delete functionality

--- app/src/main/java/com/mininote/NoteDatabaseHelper.kt
+++ app/src/main/java/com/mininote/NoteDatabaseHelper.kt
@@ -75,5 +75,12 @@ class NoteDatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_
         db.close()
         return Note(id, title, content)
     }
+    fun deleteNote(noteId: Int) {
+        val db = writableDatabase
+        val whereClause = "$COLUMN_ID = ?"
+        val whereArgs = arrayOf(noteId.toString())
+        db.delete(TABLE_NAME, whereClause, whereArgs)
+        db.close()
+    }
 
 }
