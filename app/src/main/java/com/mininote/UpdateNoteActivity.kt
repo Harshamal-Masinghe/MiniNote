67678d246e2f07fd75e0a047f5070538ce44d5f2
Harsha <it22577306@my.sliit.lk>
2024-05-11 23:14:17 +0530

    define update functionality

--- app/src/main/java/com/mininote/UpdateNoteActivity.kt
+++ app/src/main/java/com/mininote/UpdateNoteActivity.kt
@@ -1,20 +1,39 @@
 package com.mininote
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.mininote.databinding.ActivityUpdateNoteBinding
 
 class UpdateNoteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUpdateNoteBinding
+    private lateinit var db: NoteDatabaseHelper
+    private var noteId: Int = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_update)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityUpdateNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        db = NoteDatabaseHelper(this)
+
+        noteId = intent.getIntExtra("note_id", -1)
+        if (noteId == -1) {
+            finish()
+            return
+        }
+
+        val note = db.getNoteById(noteId)
+        binding.updateTitleEditText.setText(note.title)
+        binding.updateContentEditText.setText(note.content)
+
+        binding.updateSaveButton.setOnClickListener {
+            val newTitle = binding.updateTitleEditText.text.toString()
+            val newContent = binding.updateContentEditText.text.toString()
+            val updatedNote = Note(noteId, newTitle, newContent)
+            db.updateNote(updatedNote)
+            finish()
+            Toast.makeText(this, "Note updated successfully", Toast.LENGTH_SHORT).show()
         }
     }
 }
